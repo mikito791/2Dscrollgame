1427268958c2e6344de66663bb7c0d047e537f68
幹人 永澤 <230413@jc-21.jp>
2024-07-05 18:34:58 +0900

    a

--- Camera.cpp
+++ Camera.cpp
@@ -2,11 +2,11 @@
 
 void Camera::DrawDarkOverlay()
 {
-    SetDrawBlendMode(DX_BLENDMODE_ALPHA, 50); // 透明度を設定 (0-255)
+    SetDrawBlendMode(DX_BLENDMODE_ALPHA, 100); // 透明度を設定 (0-255)
     DrawBox(0, 0, 1280, 720, GetColor(0, 0, 0), TRUE); // 画面全体に黒い四角形を描画
     if (CheckHitKey(KEY_INPUT_W))
     {
-        SetDrawBlendMode(DX_BLENDMODE_ALPHA, 125);
+        SetDrawBlendMode(DX_BLENDMODE_ALPHA, 175);
         //SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0); // ブレンドモードを元に戻す
     }
 }
